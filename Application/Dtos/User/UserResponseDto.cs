b9b3739e228693a248e8b61f881c85953b7eaf79
Z1rael88 <oleksiibahmet@gmai.com>
2025-12-08 15:21:00 +0100

    Small fix

--- Application/Dtos/User/UserResponseDto.cs
+++ Application/Dtos/User/UserResponseDto.cs
@@ -5,6 +5,7 @@ namespace Application.Dtos.User;
 public class UserResponseDto
 {
     public int Id { get; set; }
+    public int Age { get; set; }
     public string Firstname { get; set; }
     public string Lastname { get; set; }
     public string Email { get; set; }
