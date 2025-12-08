c64c46f8ae974209a957c7cf726de1235a63b96a
Z1rael88 <oleksiibahmet@gmai.com>
2025-12-08 16:17:35 +0100

    small dto fix

--- Application/Dtos/Resume/ResumeResponseDto.cs
+++ Application/Dtos/Resume/ResumeResponseDto.cs
@@ -8,6 +8,7 @@ public class ResumeResponseDto
     public int Id { get; set; }
     public int? CandidateId { get; set; }
     public CandidateDto Candidate { get; set; }
+    public int ExpectedSalary { get; set; }
     public int YearsOfExperience { get; set; }
     public ICollection<LanguageLevelDto> LanguageLevels { get; set; } 
     public ICollection<JobExperienceResponseDto> JobExperiences { get; set; } 
