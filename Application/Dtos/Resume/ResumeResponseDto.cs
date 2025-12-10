269f540c4047fa80b6c738c75b26e5c4d41a8e26
Z1rael88 <oleksiibahmet@gmai.com>
2025-12-10 12:05:03 +0100

    added status in dto

--- Application/Dtos/Resume/ResumeResponseDto.cs
+++ Application/Dtos/Resume/ResumeResponseDto.cs
@@ -1,5 +1,6 @@
 using Application.Dtos.Resume.Education;
 using Application.Dtos.Resume.JobExperience;
+using Domain.Enums;
 
 namespace Application.Dtos.Resume;
 
@@ -8,6 +9,7 @@ public class ResumeResponseDto
     public int Id { get; set; }
     public int? CandidateId { get; set; }
     public CandidateDto Candidate { get; set; }
+    public ResumeStatus Status { get; set; }
     public int ExpectedSalary { get; set; }
     public int YearsOfExperience { get; set; }
     public ICollection<LanguageLevelDto> LanguageLevels { get; set; } 
